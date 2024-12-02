0a2f7d9454409c5f70f85643158906c7e0601253
dragonkiri <Raphaelmarquesmelo@gmai.com>
2024-12-02 18:56:46 -0300

    atualizaçao da pagina inicial

--- tinta.js
+++ tinta.js
@@ -1,5 +1,8 @@
 var cliente = false;
 var supply = false;
+var arquivo =false;
+var sininho = false;
+var balao = false;
 function sou_supply() {
     supply = true;
     if (supply = true) {
@@ -34,4 +37,54 @@ function Sou_tatuador(){
         document.getElementById("supply").style = "background-color: #7556568F; color: white;";
         document.body.style.backgroundImage = "url('./img/imgTatto.PNG')";
     }
+}
+function Gira_gira(el){
+el.classList.toggle('active');
+ if(arquivo == false){
+     document.getElementById('itens_arquivos').style.display="block";
+     arquivo= true;
+ } else{
+    arquivo = false;
+    document.getElementById('itens_arquivos').style.display="none";
+ }
+}
+function ativa_notifica(){
+    if(sininho == true){
+        document.getElementById('notifica').style.display="block";
+        document.getElementById('notifica').style.animationName="slide_notifica";
+        sininho =false;
+    }else{
+        document.getElementById('notifica').style.display="none";
+        sininho =true;
+        
+    }
+}
+function fecha_notifica(){
+    document.getElementById('notifica').style.animationName="out_slide_notifica";
+    sininho =true;
+}
+function mensagens(){
+    if(balao == false){
+        document.getElementById('side_papo').style.animationName="slide_papo";
+        document.getElementById('bate_papo').style.display="block";
+        balao = true;
+       
+    }else{
+        document.getElementById('side_papo').style.animationName="out_slide_papo";
+        balao = false;
+    }
+
+}
+function fecha_papo(){
+    document.getElementById('bate_papo').style.display="none";
+    balao = false;
+}
+function conversar(){
+    document.getElementById('bate_papo').style.display="none";
+    document.getElementById('conversa').style.display="block";
+}
+function fecha_conversa(){
+    document.getElementById('bate_papo').style.display="block";
+    document.getElementById('conversa').style.display="none";
+
 }
