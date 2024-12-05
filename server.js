f36389c7f9c1f7c07982348f96547931b53c59c9
dragonkiri <Raphaelmarquesmelo@gmai.com>
2024-12-04 21:41:56 -0300

    refinamento1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root', /* posivel problema na senha. testar sem ela ou com a senha senac */
-    database: 'dragontatoo'
+    database: 'blood'
 });
 
 db.connect(err => {
@@ -20,5 +20,19 @@ db.connect(err => {
         console.error('Erro ao conectar:', err);
         return;
     }
-    console.log('Conectado ao bamco');
+    console.log('Conectado ao banco');
 });
+app.get('/api/cliente',(req,res)=> {
+    db.query('SELECT * FROM CLIENTE',(err, results) => {
+        if (err){
+        console.error('erro ao buscar dados:',err);
+        res.status(500).send('Erro ao buscar dados');
+        return;
+    }
+    res.send(results);
+    });
+});
+app.post('/api/tatuador',(req,res) => {
+    con 
+})
+
